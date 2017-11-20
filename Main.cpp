10f89a7c733f33f44bd899d8688a85b7c7a3d4fc
dipirrogiovanni <gdp99@live.it>
2017-11-20 23:31:16 +0100

    Programma commentato

--- Main.cpp
+++ Main.cpp
@@ -1,9 +1,13 @@
+//Di Pirro Giovanni 5AIA 21/11/2017
 #include <iostream>
 using namespace std;
 int main(){
+	//variabile contatore
 	int c=0;
+	//ciclo da 0 a 9
 	for(int i=0;i<10;i++){
-		cout<<++c;
+		cout<<++c; //visualizzazione variabile contatore da 1 a 10
 	}
+	//pausa prima che il programma si termini
 	system("PAUSE");
 }
